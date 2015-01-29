b1447fe1221abe8ab57d28f7e1954f9887b93094
Karl C. Goedel <mail@karl-goedel.de>
2015-01-29 16:23:59 +0000

    Comments added to Zim4A.py

--- Zim4A.py
+++ Zim4A.py
@@ -5,10 +5,12 @@ import CreateHtml
 
 droid = android.Android()
 
+# Add Main Menu Items
 droid.addOptionsMenuItem('Index', 'index', None, 'ic_menu_agenda')
 droid.addOptionsMenuItem('Edit', 'edit', None, 'ic_menu_edit')
 droid.addOptionsMenuItem('Quit', 'kill', None, 'ic_menu_close_clear_cancel')
 
+# Read the path of the last opened file
 if os.path.isfile(fullpath+'currentpath.dat'):
 	f = open(fullpath+'currentpath.dat','r')
 	currentpath = f.readlines()[0]
@@ -16,6 +18,7 @@ if os.path.isfile(fullpath+'currentpath.dat'):
 else:
 	currentpath = fullpath+"welcome.txt"
 
+#Show the last opened file or show welcome page
 if os.path.isfile(fullpath+'currentzim.htm'):
     droid.webViewShow(fullpath+'currentzim.htm')
 else:
@@ -24,18 +27,23 @@ else:
     f.write(html)
     f.close()
     droid.webViewShow(fullpath+"currentzim.htm")
-        
+
+# Main event loop 
 while True:
+	# Wait for android event (from menu or javascript)
 	res = droid.eventWait().result
 	if res == None:
 		continue
+	# Quit menu button pressed
 	elif res['name'] == 'kill':
 		break
+	# Index menu button pressed
 	elif res['name'] == 'index':
 		title = 'Choose page'
 		droid.dialogCreateAlert(title)
 		filelist = []
 		pathlist = []
+		# Walk through dirdctories and generate index
 		for dirname, dirnames, filenames in os.walk(fullpath+notebook):
 			for filename in sorted(filenames):
 				if filename[-3:] == "txt":
@@ -43,6 +51,7 @@ while True:
 					filelist.append(str(os.path.join(dirname, filename))[len(fullpath+notebook)+1:-4])
 			if '.zim' in dirnames:
 				dirnames.remove('.zim')
+		# Show dialog with index
 		droid.dialogSetItems(filelist)
 		droid.dialogShow()
 		response = droid.dialogGetResponse()
@@ -60,10 +69,12 @@ while True:
 		content = CreateHtml.createcontentonly(pathlist[index],fullpath, currentpath)
 		droid.eventPost('loadnewpage', content)
 		continue
+	# Edit menu button pressed
 	elif res['name'] == 'edit':
 		content = CreateHtml.createedit(currentpath,fullpath)
 		droid.eventPost('loadnewpage', content)
 		continue
+	# Save button on edit page pressed
 	elif res['name'] == 'save':
 		changedtext = res['data']
 		f = codecs.open(currentpath,'w',encoding='utf8')
@@ -76,6 +87,7 @@ while True:
 		content = CreateHtml.createcontentonly(currentpath,fullpath,currentpath)
 		droid.eventPost('loadnewpage', content)
 		continue
+	# Cancel button on edit page pressed
 	elif res['name'] == 'cancel':
 		f = open(fullpath+"currentzim.htm","w")
 		html = CreateHtml.create(currentpath,fullpath,currentpath)
@@ -84,8 +96,10 @@ while True:
 		content = CreateHtml.createcontentonly(currentpath,fullpath,currentpath)
 		droid.eventPost('loadnewpage', content)
 		continue
+	# Link in the webview pressed
 	elif res['name'] == 'link':
 		linkpath = res['data']
+		# Create html for linked page
 		if os.path.isfile(linkpath):
 			f = open(linkpath,"w")
 			html = CreateHtml.create(linkpath,fullpath,currentpath)
@@ -94,6 +108,7 @@ while True:
 			content = CreateHtml.createcontentonly(linkpath,fullpath,currentpath)
 			droid.eventPost('loadnewpage', content)
 			continue
+		# Go back to same page, if linked page does not exist
 		else:
 			f = open(fullpath+"currentzim.htm","w")
 			html = CreateHtml.create(currentpath,fullpath,currentpath)
